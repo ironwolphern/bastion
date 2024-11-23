d03bc9ccf861af4059e1e70a18c47dbe08406e5f
Fernando Hernández San Felipe <ironwolphern@iWolphPortPRO.local>
2024-11-23 12:57:31 +0100

    Initial commit

--- distribution/python/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-docker buildx build -t bastion:0.1.0-python \
-    --build-arg PYTHON_VERSION=3.13.0-slim \
-    --build-arg TERRAFORM_VERSION=1.9.8 \
-    --build-arg OC_VERSION=4.17.5 \
-    -f ./Dockerfile .
