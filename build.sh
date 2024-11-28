0225e8d233faaca6fe38b9314c88b3820ad64d9e
Fernando Hernández San Felipe <ironwolphern@outlook.com>
2024-11-28 19:16:54 +0100

    Fix workflows and add improvements

--- build.sh
+++ build.sh
@@ -1,27 +1,25 @@
 #!/bin/bash
 # Version tools
-PYTHON=3.13.0
-TERRAFORM=1.9.8
-OC=4.17.5
+export $(cat .env | xargs)
 
 # Build bastion image fot python
 docker buildx build -t bastion:0.1.0-python \
-    --build-arg PYTHON_VERSION=${PYTHON}-slim \
-    --build-arg TERRAFORM_VERSION=${TERRAFORM} \
-    --build-arg OC_VERSION=${OC} \
+    --build-arg PYTHON_VERSION=${PYTHON_VERSION}-slim \
+    --build-arg TERRAFORM_VERSION=${TERRAFORM_VERSION} \
+    --build-arg OC_VERSION=${OC_VERSION} \
     -f ./distribution/python/Dockerfile .
 docker builder prune -f -a
 
 # Build bastion image for rhel
 docker buildx build -t bastion:0.1.0-rhel \
-    --build-arg TERRAFORM_VERSION=${TERRAFORM} \
-    --build-arg OC_VERSION=${OC} \
+    --build-arg TERRAFORM_VERSION=${TERRAFORM_VERSION} \
+    --build-arg OC_VERSION=${OC_VERSION} \
     -f ./distribution/rhel/Dockerfile .
 docker builder prune -f -a
 
 # Build bastion image for ubuntu
 docker buildx build -t bastion:0.1.0-ubuntu \
-    --build-arg TERRAFORM_VERSION=${TERRAFORM} \
-    --build-arg OC_VERSION=${OC} \
+    --build-arg TERRAFORM_VERSION=${TERRAFORM_VERSION} \
+    --build-arg OC_VERSION=${OC_VERSION} \
     -f ./distribution/ubuntu/Dockerfile .
 docker builder prune -f -a
