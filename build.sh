f8273d12487a891b4fd86cf108a2d9fbd148683c
Fernando Hernández San Felipe <ironwolphern@outlook.com>
2024-12-02 21:49:49 +0100

    Add optimizations workflows and add improvements

--- build.sh
+++ build.sh
@@ -3,23 +3,34 @@
 export $(cat .env | xargs)
 
 # Build bastion image fot python
-docker buildx build -t bastion:0.1.0-python \
-    --build-arg PYTHON_VERSION=${PYTHON_VERSION}-slim \
+docker buildx build -t ironwolphern/bastion-python:test \
     --build-arg TERRAFORM_VERSION=${TERRAFORM_VERSION} \
     --build-arg OC_VERSION=${OC_VERSION} \
+    --build-arg GITHUB_SHA="b487c04a8e7b67f70efc5da751d9f1184cabfbc5" \
+    --build-arg GITHUB_REF="refs/heads/develop" \
+    --build-arg GITHUB_REPOSITORY="ironwolphern/bastion" \
+    --build-arg GITHUB_ACTOR="ironwolphern" \
     -f ./distribution/python/Dockerfile .
 docker builder prune -f -a
 
 # Build bastion image for rhel
-docker buildx build -t bastion:0.1.0-rhel \
+docker buildx build -t ironwolphern/bastion-rhel:test \
     --build-arg TERRAFORM_VERSION=${TERRAFORM_VERSION} \
     --build-arg OC_VERSION=${OC_VERSION} \
+    --build-arg GITHUB_SHA="b487c04a8e7b67f70efc5da751d9f1184cabfbc5" \
+    --build-arg GITHUB_REF="refs/heads/develop" \
+    --build-arg GITHUB_REPOSITORY="ironwolphern/bastion" \
+    --build-arg GITHUB_ACTOR="ironwolphern" \
     -f ./distribution/rhel/Dockerfile .
 docker builder prune -f -a
 
 # Build bastion image for ubuntu
-docker buildx build -t bastion:0.1.0-ubuntu \
+docker buildx build -t ironwolphern/bastion-ubuntu:test \
     --build-arg TERRAFORM_VERSION=${TERRAFORM_VERSION} \
     --build-arg OC_VERSION=${OC_VERSION} \
+    --build-arg GITHUB_SHA="b487c04a8e7b67f70efc5da751d9f1184cabfbc5" \
+    --build-arg GITHUB_REF="refs/heads/develop" \
+    --build-arg GITHUB_REPOSITORY="ironwolphern/bastion" \
+    --build-arg GITHUB_ACTOR="ironwolphern" \
     -f ./distribution/ubuntu/Dockerfile .
 docker builder prune -f -a
