ce7e74dbc89389dfe64ec64f1b1088a33c58deb6
Fernando Hernández San Felipe <ironwolphern@outlook.com>
2024-12-07 18:29:15 +0100

    Add improvements

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 # Function to verify whether configuration directories exist
 check_config_dirs() {
-    # Kubernetes config directory
+    # Kubernetes config
     if [ -d "/config/.kube" ]; then
         cp -r /config/.kube/* ~/.kube/
     fi
 
-    # Terraform config directory
+    # Terraform config
     if [ -d "/config/.terraform.d" ]; then
         cp -r /config/.terraform.d/* ~/.terraform.d/
     fi
 
-    # Ansible config directory
+    # Ansible config
     if [ -d "/config/.ansible" ]; then
         cp -r /config/.ansible/* ~/.ansible/
     fi
